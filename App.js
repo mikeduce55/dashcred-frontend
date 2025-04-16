86ef46378e9b9cfe2c53595737fde9b3ef48820d
mikeduce55 <mikeduce55@gmail.com>
2025-04-15 20:40:42 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,1 @@
 
-import React from 'react';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>DashCred is live</h1>
-      <p>Convert / Store / Trade UI will appear here</p>
-    </div>
-  );
-}
-
-export default App;
