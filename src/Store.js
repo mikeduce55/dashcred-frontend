3a4ca8bb71e8e228f3dce3a4eb42455eb3677048
mikeduce55 <mikeduce55@gmail.com>
2025-04-15 21:05:50 -0400

    Add files via upload

---
+++ src/Store.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Store() {
+  return (
+    <div>
+      <h2>Store Gift Card</h2>
+      <p>Securely store your gift card here for later use or trade.</p>
+    </div>
+  );
+}
+
+export default Store;
