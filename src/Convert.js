fe841511639001089cb936e6ca03adea7bbdb32a
mikeduce55 <mikeduce55@gmail.com>
2025-04-15 21:05:13 -0400

    Add files via upload

---
+++ src/Convert.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Convert() {
+  return (
+    <div>
+      <h2>Convert Gift Card</h2>
+      <p>Enter your gift card details here to convert to DoorDash credit.</p>
+    </div>
+  );
+}
+
+export default Convert;
